b9e533ca4c8b4e21e73b7888e629f392a6e6ddec
Edikan Odokwo <edetedikan647@gmail.com>
2025-06-05 21:44:39 +0100

    Add about and home pages, update Header with navigation links

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,9 @@
+const About: React.FC = () => {
+    return (
+        <div className="p-8">
+      <h1 className="text-2xl font-bold">About Us</h1>
+    </div>
+    )
+}
+
+export default About
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,9 @@
+const HomePage: React.FC = () => {
+    return (
+       <div className="p-8">
+      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+    </div>
+    )
+}
+
+export default HomePage
